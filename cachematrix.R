9a98bd18a24e3f154c5f816cff5b270bf03c2e2f
nohut <balipelin@gmail.com>
2016-03-24 23:33:48 -0400

    My Changes
    Updates for the programming assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## Function: makeCacheMatrix
+## Param x: matrix
+##
+## Creates a special matrix object that caches the  
+## given x matrix value and its inverse 
+##
 makeCacheMatrix <- function(x = matrix()) {
-
+	i <- NULL
+	set <- function(y) {
+		x <<- y
+		i <<- NULL
+	}
+	get <- function() x
+	setinv <- function(inv) i <<- inv
+	getinv <- function() i
+	
+	list(set = set, get= get, setinv = setinv, getinv = getinv)
 }
+## Function: cacheSolve
+## param x: matrix
+## Calculates the inverse of the given x value if it is not null.
+## Otherwise, it reads from the cache
+##
+##
 
+cacheSolve <- function(x) {
+      ## Return a matrix that is the inverse of 'x'
+	i <- x$getinv()
+	data <- x$get()
 
-## Write a short comment describing this function
+	## Check if the inverse of the data is not null and
+	## if the matrix has been changed.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	if (!is.null(i) && !is.null(prevdata) && !is.null(data) && dim(prevdata) == dim(data) && all(prevdata == data)) {
+		message("getting cached data.")
+		return(i)
+	}
+
+	prevdata <<- data
+	i <- solve(data)
+	x$setinv(i)
+	i
 }
+	
